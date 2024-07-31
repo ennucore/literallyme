00d563c43644136a4a9eafcd5fad6b12c233e216
Lev <lc@lev.la>
2024-07-31 15:25:25 -0700

    requirements and setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ./get_models.sh
-sudo apt update && sudo apt upgrade -y && sudo apt install -y python-is-python3 python3-pip ffmpeg unzip python3-venv
+sudo apt update && sudo apt upgrade -y && sudo apt install -y python-is-python3 python3-pip ffmpeg unzip python3-venv libcudnn9-cuda-12
 pip install pipx
 pipx ensurepath
 pipx install poetry
+pipx run poetry install
 
