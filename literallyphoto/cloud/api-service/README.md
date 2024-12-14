59cecd5e7a98e35e7496573be82bd7f56946b957
hellesgrind <alekseis.naumov@gmail.com>
2024-12-14 17:53:54 +0400

    WIP: add start_training to api-service

--- literallyphoto/cloud/api-service/README.md
+++ literallyphoto/cloud/api-service/README.md
@@ -6,7 +6,31 @@ Main entrypoint of service that handles `training` and `generate_photo` requests
 - Build: `npm install`
 - Run locally: `node index.js`. It will be served at http://localhost:8080
 
+
 # Deploy instructions (not working until google cloud project is set up)
 
-- `gcloud builds submit --tag  us-central1-docker.pkg.dev/literallyme/literallyme-main-repo/api-service`
-- `gcloud run deploy api-service --image us-central1-docker.pkg.dev/literallyme/literallyme-main-repo/api-service --allow-unauthenticated --service-account default-account@literallyme.iam.gserviceaccount.com`
+## Build
+```
+gcloud builds submit --tag us-central1-docker.pkg.dev/literallyme-dev/literallyme-main-repo/api-service
+```
+
+## Deploy
+
+```
+gcloud run deploy api-service --image us-central1-docker.pkg.dev/literallyme-dev/literallyme-main-repo/api-service --allow-unauthenticated --service-account default-account@literallyme.iam.gserviceaccount.com
+```
+
+## Deploy locally
+
+```
+PORT=8080 && \
+docker pull us-central1-docker.pkg.dev/literallyme-dev/literallyme-main-repo/api-service:latest && \
+docker run -p 9090:${PORT} -e PORT=${PORT} us-central1-docker.pkg.dev/literallyme-dev/literallyme-main-repo/api-service:latest
+```
+
+## Build and run locally
+
+```
+PORT=8080 && node index.js
+```
+
--- literallyphoto/cloud/api-service/index.js
+++ literallyphoto/cloud/api-service/index.js
@@ -1,11 +1,52 @@
-'use strict';
+`use strict`;
 
-const app = require('./app');
+const express = require('express');
+const { ExecutionsClient } = require('@google-cloud/workflows');
+// const { firestore } = require('./firebase');
 
+const TRAINING_WORKFLOW_NAME = 'workflow-training';
+const PROJECT_ID = 'literallyme-dev';
 const PORT = parseInt(process.env.PORT) || 8080;
 
-const main = async () => {
-  app.listen(PORT);
-};
+const executionsClient = new ExecutionsClient();
 
-main();
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+app.use(express.raw({ type: 'application/zip', limit: '1Gb' }));
+app.use(express.json());
+
+// TODO: Add auth
+app.post(`/start_training`, async (req, res) => {
+  const { userId, targetId, archiveUrl } = req.body;
+  console.log(
+    `Received request to start training userId ${userId} targetId ${targetId} archiveUrl ${archiveUrl}`
+  );
+  // TODO: Add checking for sufficient balance
+  const input = {
+    userId: userId,
+    archiveUrl: archiveUrl,
+  };
+  console.log(`Training input: ${JSON.stringify(input)}`);
+  // TODO: Add writing to DB
+  // await firestore.collection('trainings').doc(userId).collection('targets').doc(targetId).set({
+  //   weightsUrl: '',
+  //   callbacks: [],
+  //   status: 'processing',
+  // });
+  const workflow = executionsClient.workflowPath(PROJECT_ID, 'us-central1', TRAINING_WORKFLOW_NAME);
+
+  const executionResponse = await executionsClient.createExecution({
+    parent: workflow,
+    execution: {
+      argument: JSON.stringify(input),
+    },
+  });
+  console.log(executionResponse);
+  res.status(200).json({ status: 'success' });
+});
+
+app.post(`/generate_photos`, async (req, res) => {
+  pass;
+});
